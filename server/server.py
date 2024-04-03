1eaf02b4b6aba92c995999d5da42094f4ffa4abe
Shantanu <shantanuwable2003@gmail.com>
2024-04-03 15:47:11 +0530

    refactor: move spam filter logic into own file

--- server/server.py
+++ server/server.py
@@ -1,54 +1,14 @@
 from flask import Flask, jsonify, request, Response
 from flask_cors import CORS
 import os
-from lib.listen import listen_for_emails
 from threading import Thread
-from pydantic import BaseModel
-from joblib import load
-import nltk
-import string
-import re
 from dotenv import load_dotenv
 load_dotenv()
+from lib.listen import listen_for_emails
+from lib.spam_filter import preprocess_text, spam_model, EmailInput
 
 app = Flask(__name__)
 CORS(app)
-
-#pre-processing
-stop_words = set(nltk.corpus.stopwords.words("english"))
-spetial_chars = set(string.printable) - set(string.ascii_letters) - set(" ")
-escaped_chars = [re.escape(c) for c in spetial_chars]
-regex = re.compile(f"({'|'.join(escaped_chars)})")
-stemmer = nltk.stem.porter.PorterStemmer()
-url_regex = re.compile("(?P<url>https?://[^\s]+)")
-
-class EmailInput(BaseModel):
-    email_text: str
-
-#loading the model
-model_path = "lib/spam_detection_model.joblib"
-spam_model = load(model_path)
-
-def preprocess_text(text):
-    # capitalization
-    text = text.lower()
-
-    # remove urls
-    text = re.sub(url_regex," ",text)
-    
-    # tokenization
-    text = nltk.word_tokenize(text, language='english')
-        
-    # stop words removal
-    text = [word for word in text if word not in stop_words]
-    
-    # noise removal
-    text = [word for word in text if word.isalpha()]
-    
-    # stemming
-    text = [stemmer.stem(word) for word in text]
-    
-    return ' '.join(text)
 
 @app.route('/email_data', methods=['POST'])
 def receive_email_data():
@@ -64,6 +24,16 @@ def receive_email_data():
 
 @app.post("/predict")
 def detect_spam():
+    """
+    Function to process an email and determine if it is spam or not.
+
+    `body` of the request must be of the format:
+    ```json
+    {
+        "email_text": "your email contents"
+    }
+    ```
+    """
     try:
         data = request.json
         email_input = EmailInput(**data)
@@ -71,11 +41,11 @@ def detect_spam():
         prediction = spam_model.predict([email_text])
         is_spam = bool(prediction[0])
 
-        return {"is_spam": is_spam}
+        return jsonify({"is_spam": is_spam})
     except:
         return Response("Invalid input", status=400)
 
-if __name__ == "__main__":
+def start():
     # Email listening in a background thread
     email_thread = Thread(
         target=listen_for_emails, 
@@ -87,5 +57,8 @@ if __name__ == "__main__":
         )
     )
     email_thread.start()
-    
-    app.run(debug=True)
+
+    # Keeping `debug=True` spawns two threads which monitor the same email address
+    app.run(debug=False)
+
+start()
