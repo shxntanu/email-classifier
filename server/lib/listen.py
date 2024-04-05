d3f1ea0d1a8395df8afa42f5169d607d3f52d98c
Shantanu <shantanuwable2003@gmail.com>
2024-04-05 23:47:12 +0530

    feat: add function to listen for and return raw emails

--- server/lib/listen.py
+++ server/lib/listen.py
@@ -43,5 +43,34 @@ def listen_for_emails(imap_server, imap_port, email_address, password, url):
         print("Waiting for new emails...")
         time.sleep(10)
 
+def listen_raw_emails(imap_server, imap_port, email_address, password, url):
+    """
+    Listens for incoming emails on the specified email account and returns the raw email data.
+    """
+
+    imap = imaplib.IMAP4_SSL(imap_server, imap_port)
+    imap.login(email_address, password)
+   
+    while True:
+        print("Checking for new emails...")
+        imap.select('INBOX')
+        typ, data = imap.search(None, '(UNSEEN)')
+
+        if typ == 'OK':
+            for num in data[0].split():
+                typ, msg_data = imap.fetch(num, '(RFC822)')
+                if typ == 'OK':
+                    raw_email = msg_data[0][1]
+                    email_message = email.message_from_bytes(raw_email)
+
+                    # Hit an endpoint with the latest email data
+                    data = {'raw_email': email_message.as_string()}
+                    response = requests.post(url, json=data)
+                    print("Response:", response.text)
+
+        print("Waiting for new emails...")
+        time.sleep(10)
+
+
 # Example usage
 # listen_for_emails('imap.gmail.com', 993, os.environ["EMAIL_ID"], os.environ["EMAIL_APP_PASSWORD"])
